5b6a6192d964a8e7943f7a2b10c907d4df59e2ab
mmallette <melmalle90@gmail.com>
2014-07-15 06:56:52 -0400

    Update processLogin.php

--- processLogin.php
+++ processLogin.php
@@ -1,2 +1,24 @@
 <?php
 
+include_once 'dbconnect.php';
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+include_once 'functions.php';
+ 
+sec_session_start(); // Our custom secure way of starting a PHP session.
+ 
+if (isset($_POST['email'], $_POST['p'])) {
+    $email = $_POST['email'];
+    $password = $_POST['p']; // The hashed password.
+ 
+    if (login($email, $password, $mysqli) == true) {
+        // Login success 
+        header('Location: ../blog.php');
+    } else {
+        // Login failed 
+        header('Location: ../index.php?error=1');
+    }
+} else {
+    // The correct POST variables were not sent to this page. 
+    echo 'Invalid Request';
+}
+
