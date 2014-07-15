07e866b1a05e7dff78ce7f276555f9b15ad9ff6e
mmallette <melmalle90@gmail.com>
2014-07-15 06:58:47 -0400

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+    define("HOST", "localhost");    
+    define("USER", "root");    
+    define("PASSWORD", "");   
+    define("DATABASE", "floridaFishing");    
+    define("CAN_REGISTER", "any");
+    define("DEFAULT_ROLE", "member");
+    define("SECURE", FALSE);
+
+
+
+
+
+?>
