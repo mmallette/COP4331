f41cb6478746f4eda79b4d9e2ce353830b064bae
mmallette <melmalle90@gmail.com>
2014-07-15 06:55:08 -0400

    Update joinMember.php

--- joinMember.php
+++ joinMember.php
@@ -1,78 +1,106 @@
 <?php
-    
+    include_once 'dbconnect.php';   // As functions.php is not included
+    $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+include_once 'register.inc.php';
+include_once 'functions.php';
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>Florida Fishing</title>
+    <script type="text/JavaScript" src="js/sha512.js"></script> 
+    <script type="text/JavaScript" src="js/forms.js"></script>
     <link href="css/style.css" rel="stylesheet">
   </head>
-  
-<!-- NAVBAR ================================================== -->
-  <body>
-    <div class="navbar-wrapper">
-      <div class="container">
+    <body>
+        <div class="navbar-wrapper">
+        <div class="container">
 
-        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-          <div class="container">
-            <div class="navbar-header">
-              <a class="navbar-brand" href="index.php">Fishing Florida</a>
+          <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container">
+              <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Fishing Florida</a>
+              </div>
+              <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="spots.html">Spots</a></li>
+                  <li><a href="alvaro.html">Alvaro</a></li>
+                  <li><a href="kevin.html">Jokes</a></li>
+                  <li><a href="ibrahim.html">Tides</a></li>
+                  <li><a href="blog.php">Blog</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="login.php">Login</a></li>
+                  <li class="active"><a href="joinMember.php">Become a Member</a></li>
+                </ul>
+              </div>
             </div>
-            <div class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="spots.html">Spots</a></li>
-                <li><a href="alvaro.html">Alvaro</a></li>
-                <li><a href="kevin.html">Jokes</a></li>
-                <li><a href="ibrahim.html">Tides</a></li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="login.php">Login</a></li>
-                <li class="active"><a href="joinMember.php">Become a Member</a></li>
-              </ul>
-            </div>
+          </div>
+
+        </div>
+        </div>
+        
+        <div class="container marketing">
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Join Today! <span class="text-muted">Gain access to the stories and advice of other fishermen!</span></h2>
+                <p class="lead">Please enter the information below to join.</p>
+                <ul>
+                <li>Usernames may contain only digits, upper and lower case letters and underscores</li>
+                <li>Emails must have a valid email format</li>
+                <li>Passwords must be at least 6 characters long</li>
+                <li>Passwords must contain
+                    <ul>
+                        <li>At least one upper case letter (A..Z)</li>
+                        <li>At least one lower case letter (a..z)</li>
+                        <li>At least one number (0..9)</li>
+                    </ul>
+                </li>
+                <li>Your password and confirmation must match exactly</li>
+            </ul>
+            <form action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>" 
+                    method="post" 
+                    name="registration_form">
+                Username: <input type='text' 
+                    name='username' 
+                    id='username' /><br>
+                Email: <input type="text" name="email" id="email" /><br>
+                Password: <input type="password"
+                                 name="password" 
+                                 id="password"/><br>
+                Confirm password: <input type="password" 
+                                         name="confirmpwd" 
+                                         id="confirmpwd" /><br>
+                <input type="button" 
+                       value="Register" 
+                       onclick="return regformhash(this.form,
+                                       this.form.username,
+                                       this.form.email,
+                                       this.form.password,
+                                       this.form.confirmpwd);" /> 
+            </form>
+            <p>Return to the <a href="login.php">login page</a>.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-responsive" src="http://stockfresh.com/files/a/anatolym/m/18/144240_stock-photo-3d-small-people---fisherman-and-fish.jpg" alt="Generic placeholder image">
           </div>
         </div>
 
+        <hr class="featurette-divider">
+
+        <!-- FOOTER -->
+        <footer>
+
+          <p>&copy; 2014 Florida Fishing, Inc. &middot; <a href="terms.html">Terms</a></p>
+        </footer>
+
       </div>
-    </div>
-
-    <!-- Main Page Content
-    ================================================== -->
-
-    <div class="container marketing">
-
-      
-      <!-- here is the content-->
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Join Today! <span class="text-muted">Gain access to the stories and advice of other fishermen!</span></h2>
-          <p class="lead">Please enter the information below to join.</p>
-          <form class="pad" name="joinInfo" action="processLogin.php" method="post">
-            Username: <input type="text" name="username" maxlength="20" /> <br />
-            Password:  <input type="text" name="password" maxlength="20" /> <br />
-            Confirm Password: <input type="text" name="cPassword" maxlength="20" /> <br />
-            <input type="submit" name="submit" value="Submit Join Info" />
-          </form>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive" src="http://stockfresh.com/files/a/anatolym/m/18/144240_stock-photo-3d-small-people---fisherman-and-fish.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <!-- FOOTER -->
-      <footer>
         
-        <p>&copy; 2014 Florida Fishing, Inc. &middot; <a href="terms.html">Terms</a></p>
-      </footer>
-
-    </div><!-- /.container -->
-
+        
+    </body>
+</html>
 
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
